bb58654efa6a4809bce7264029475d6bb8e70d48
JohnnySimple <obengboatengjohnson@gmail.com>
2023-04-14 16:57:59 +0000

    update: update layout

--- src/views/DashboardView.vue
+++ src/views/DashboardView.vue
@@ -1,9 +1,9 @@
 <template>
   <main class="flex">
-    <div class="w-1/12">
+    <div class="w-2/12">
       <Sidebar />
     </div>
-    <div class="w-11/12">
+    <div class="w-10/12">
       <RouterView />
     </div>
   </main>
