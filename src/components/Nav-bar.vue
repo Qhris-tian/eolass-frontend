00d62820e5c7be8d87d07910cde73cb3ede06b86
JohnnySimple <obengboatengjohnson@gmail.com>
2023-04-14 00:10:22 +0000

    add: add initial navbar design

---
+++ src/components/Nav-bar.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="flex justify-between">
+        <div class="">
+            <SvgComponent :name=iconName class="eneba-svg" />
+        </div>
+        <div class="">
+            <SearchBarComponent />
+        </div>
+        <div class="">
+            
+        </div>
+    </div>
+</template>
+
+<script setup lang="ts">
+import SearchBarComponent from './ui/SearchBarComponent.vue';
+import SvgComponent from './ui/SvgComponent.vue'
+
+const props = defineProps<{
+    iconName: string
+}>()
+
+</script>
+
+<style scoped>
+</style>
--- src/views/dashboard/ProductView.vue
+++ src/views/dashboard/ProductView.vue
@@ -1,11 +1,14 @@
 <template>
     <div>
+        <Navbar iconName="eneba" />
+    </div>
+    <div>
         Products
     </div>
 </template>
 
 <script setup lang="ts">
-
+import Navbar from '@/components/Nav-bar.vue'
 </script>
 
 <style scoped>
