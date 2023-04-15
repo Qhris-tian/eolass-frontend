3269ca4dcb6f98c0966e72abbf2c783d669aa77d
JohnnySimple <obengboatengjohnson@gmail.com>
2023-04-15 16:56:36 +0000

    add: add initial product card template

---
+++ src/components/ui/ProductCard.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="w-4/12 md:w-1/5 h-[384px] mb-2 pr-2 pb-10">
+        <div class="shadow h-full">
+            <div class="h-3/5">
+                Image
+            </div>
+            <div class="h-2/5">
+                <div class="h-[70%]">
+                    Text
+                </div>
+                <div class="flex justify-center p-1">
+                    <button class="bg-[#635FC7] py-2 px-7 rounded hover:border border-[#E4EBFA] text-white text-[12px] font-medium flex justify-center items-center">
+                        <span>Auction</span>
+                    </button>
+                </div>
+            </div>
+            
+        </div>
+    </div>
+</template>
+
+<script setup lang="ts">
+</script>
+
+<style scoped>
+</style>
--- src/views/dashboard/ProductView.vue
+++ src/views/dashboard/ProductView.vue
@@ -1,12 +1,26 @@
 <template>
    <div>
         <Navbar iconName="eneba" />
-        Products
+        <div class="mt-10">
+            <header>
+               <span>Products</span>
+            </header>
+        </div>
+        <div class="flex flex-wrap">
+            <ProductCard />
+            <ProductCard />
+            <ProductCard />
+            <ProductCard />
+            <ProductCard />
+            <ProductCard />
+            <ProductCard />
+        </div>
    </div>
 </template>
 
 <script setup lang="ts">
 import Navbar from '@/components/Nav-bar.vue'
+import ProductCard from '@/components/ui/ProductCard.vue';
 </script>
 
 <style scoped>
