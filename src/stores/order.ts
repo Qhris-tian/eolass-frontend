c0a5ee621187cb16ba0ad958f4f198ee28145054
Qhris-tian <christian.kojobarnes@gmail.com>
2023-04-15 21:06:12 +0000

    Create order.ts

---
+++ src/stores/order.ts
@@ -0,0 +1,14 @@
+import { ref } from 'vue'
+import { defineStore } from 'pinia'
+import orderResponse from './json/order_history_response.json'
+
+export const useOrderStore = defineStore('order', () => {
+    const orderHistory = ref({})
+
+    // function to make api request for order history api data.
+    function getOrderHistoryData() {    
+      orderHistory.value = orderResponse.results
+    }
+
+  return { orderHistory, getOrderHistoryData }
+})
