910e6b965a3c614eb4150bd5af7eae6aeaaf1f46
JohnnySimple <obengboatengjohnson@gmail.com>
2023-04-16 00:40:00 +0000

    add: add create auction modal template

---
+++ src/components/Create-auction-modal.vue
@@ -0,0 +1,41 @@
+<template>
+
+  <div v-if="isOpen" class="modal-mask fixed inset-0 bg-[rgba(0,0,0,0.6)] z-50 grid place-items-center">
+    <div class="modal-container bg-white p-4 w-[80vw] max-w-[700px]">
+        <header class="">
+            <div class="flex justify-between font-medium">
+                <div class="w-full flex justify-center text-center">
+                    Create Auction<br>
+                    {{ title }}
+                </div>
+                <div class="">
+                    <img src="@/assets/close-icon.svg" @click="$emit('closeModal')" class="cursor-pointer" width="15" height="15" alt="">
+                </div>
+            </div>
+        </header>
+        <div class="font-medium text-sm pt-5">
+            body
+        </div>
+        <!-- <footer>
+            <button 
+                @click="$emit('closeModal')"
+                class="bg-[#E4EBFA] font-meduim text-sm py-3 px-5 rounded hover:border border-[#E4EBFA] text-[#828FA3]">
+                close
+            </button>
+        </footer> -->
+    </div>
+  </div>
+
+</template>
+
+<script setup lang="ts">
+
+defineProps({
+    isOpen: Boolean,
+    title: String
+})
+
+</script>
+
+<style scoped>
+</style>
--- src/views/dashboard/AuctionView.vue
+++ src/views/dashboard/AuctionView.vue
@@ -8,19 +8,26 @@
         </div>
         <div>
             <div class="w-11/12 pt-5 flex justify-end">
-                <button class="bg-[#635FC7] py-3 px-5 mb-5 rounded hover:border border-[#E4EBFA] text-white text-[12px] font-medium flex justify-center items-center">
+                <button @click="showCreateAuctionModal = true" class="bg-[#635FC7] py-3 px-5 mb-5 rounded hover:border border-[#E4EBFA] text-white text-[12px] font-medium flex justify-center items-center">
                     <img src="@/assets/plus.svg" width="23" height="23" class="pr-2" alt="">
                     <span>Create Auction</span>
                 </button>
             </div>
             <AuctionTable />
         </div>
+        <CreateAuctionModal :isOpen="showCreateAuctionModal" @closeModal="showCreateAuctionModal = false"
+            title="Call of Duty: Modern Warfare II" />
     </div>
 </template>
 
 <script setup lang="ts">
-import Navbar from '@/components/Nav-bar.vue'
+import { ref } from 'vue';
+import Navbar from '@/components/Nav-bar.vue';
 import AuctionTable from '@/components/Auction-table.vue';
+import CreateAuctionModal from '@/components/Create-auction-modal.vue';
+
+let showCreateAuctionModal = ref(false);
+
 </script>
 
 <style scoped>
