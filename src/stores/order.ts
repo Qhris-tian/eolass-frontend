f911ce09568f03ca9ab2672acffaf638aa18bee6
Qhris-tian <christian.kojobarnes@gmail.com>
2023-04-16 12:39:51 +0000

    Update order.ts

--- src/stores/order.ts
+++ src/stores/order.ts
@@ -1,14 +1,21 @@
 import { ref } from 'vue'
 import { defineStore } from 'pinia'
 import orderResponse from './json/order_history_response.json'
+import orderCardsResponse from './json/order_cards_response.json'
 
 export const useOrderStore = defineStore('order', () => {
-    const orderHistory = ref({})
-
+  const orderHistory = ref({})
+  function getOrderHistoryData() {
     // function to make api request for order history api data.
-    function getOrderHistoryData() {    
-      orderHistory.value = orderResponse
-    }
+    orderHistory.value = orderResponse
+  }
 
-  return { orderHistory, getOrderHistoryData }
+  const cards = ref({})
+  function getOrderCards(reference: string) {
+    if (typeof (cards.value[reference]) === 'undefined') {      
+      cards.value[reference] = orderCardsResponse.results
+    }
+  }
+
+  return { orderHistory, cards, getOrderHistoryData, getOrderCards }
 })
