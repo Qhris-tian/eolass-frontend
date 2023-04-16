fe6504946ca40b943748ecca921108bc9bb713fd
JohnnySimple <obengboatengjohnson@gmail.com>
2023-04-16 13:52:20 +0000

    add: add create auction form template

--- src/components/Create-auction-modal.vue
+++ src/components/Create-auction-modal.vue
@@ -14,15 +14,52 @@
             </div>
         </header>
         <div class="font-medium text-sm pt-5">
-            body
+            <div class="flex justify-center">
+                <div class="w-full">
+                    <form action="" @submit="createAuction()">
+                        <div class="flex">
+                            <div class="mb-4 px-2 w-full md:w-6/12">
+                                <label for="auctionType" class="block mb-1 text-sm">Auction Type:</label>
+                                <select id="auctionType" class="w-full border px-4 pr-8 py-2 rounded focus:border-blue-500 focus:shadow-outline outline-none">
+                                    <option value="plain">Plain</option>
+                                    <option value="preorder">Pre Order</option>
+                                    <option value="declaredstock">Declared Stock</option>
+                                </select>
+                            </div>
+                            <div class="w-full md:w-6/12 flex items-center md:pl-20">
+                                <label for="autoRenew" class="block mb-1 text-sm">AutoRenew:</label>
+                                <input type="checkbox" class="w-4 h-4 ml-5 rounded focus:ring-blue-500">
+                            </div>
+                        </div>
+
+                        <div class="mb-4 px-2 w-full md:w-6/12">
+                            <label for="amount" class="block mb-1 text-sm">Amount:</label>
+                            <div class="flex flex-row justify-between items-center">
+                                <div class="mb-4 px-2 basis-1/4">
+                                    <select id="currency" class="w-full border px-4 pr-8 py-2 rounded focus:border-blue-500 focus:shadow-outline outline-none">
+                                        <option value="EUR">EUR</option>
+                                        <option value="USD">USD</option>
+                                    </select>
+                                </div>
+                                <div class="mb-4 px-2 basis-3/4">
+                                    <input id="amount" required class="w-full border px-4 py-2 rounded focus:border-blue-500 focus:shadow-outline outline-none" type="text" autofocus placeholder="Amount" />
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="mb-4 px-2 w-full md:w-6/12">
+                            <label for="keys" class="block mb-1 text-sm">Keys:</label>
+                            <input id="keys" required class="w-full border px-4 py-2 rounded focus:border-blue-500 focus:shadow-outline outline-none" type="text" autofocus placeholder="Enter key" />
+                        </div>
+                        
+                        <div class="flex justify-end px-10">
+                            <button type="submit" class="focus:outline-none text-white bg-[#635FC7] hover:bg-[#635FC7] focus:ring-2 focus:ring-[#635FC7] font-medium rounded-lg text-sm px-5 py-2.5 mb-2 dark:bg-[#635FC7] dark:hover:bg-[#A8A4FF] dark:focus:ring-[#635FC7]">Proceed</button>
+                        </div>
+
+                    </form>
+                </div>
+            </div>
         </div>
-        <!-- <footer>
-            <button 
-                @click="$emit('closeModal')"
-                class="bg-[#E4EBFA] font-meduim text-sm py-3 px-5 rounded hover:border border-[#E4EBFA] text-[#828FA3]">
-                close
-            </button>
-        </footer> -->
     </div>
   </div>
 
@@ -34,6 +71,10 @@ defineProps({
     isOpen: Boolean,
     title: String
 })
+
+function createAuction() {
+    alert('creating auction...')
+}
 
 </script>
 
