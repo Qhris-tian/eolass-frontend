b2c5389aa032e0b30e0dfeaa6614020c28fab082
JohnnySimple <obengboatengjohnson@gmail.com>
2023-04-19 02:02:26 +0000

    update: add updates

--- src/components/Update-auction-modal.vue
+++ src/components/Update-auction-modal.vue
@@ -1,12 +1,12 @@
 <template>
 
-    <div v-if="isUpdateAuctionModalOpen" class="modal-mask fixed inset-0 bg-[rgba(0,0,0,0.6)] z-50 grid place-items-center">
+    <div v-if="props.isUpdateAuctionModalOpen" class="modal-mask fixed inset-0 bg-[rgba(0,0,0,0.6)] z-50 grid place-items-center">
         <div class="modal-container bg-white p-4 w-[80vw] max-w-[700px]">
             <header class="">
                 <div class="flex justify-between font-medium">
                     <div class="w-full flex justify-center text-center">
                         Update Auction for<br>
-                        {{ auctionData?.node.product.name }}
+                        {{ props.auctionData?.node.product.name }}
                     </div>
                     <div class="">
                         <img src="@/assets/close-icon.svg" @click="$emit('closeUpdateAuctionModal')" class="cursor-pointer" width="15" height="15" alt="">
@@ -37,7 +37,7 @@
                                     <label for="autoRenew" class="block mb-1 text-sm mr-2">AutoRenew:</label>
 
                                     <label class="relative inline-flex items-center cursor-pointer">
-                                    <input type="checkbox" value="" class="sr-only peer">
+                                    <input type="checkbox" v-model="autoRenew" value="" class="sr-only peer">
                                     <div class="w-11 h-6 bg-[#ccc] peer-focus:outline-none rounded-full peer dark:bg-[#A8A4FF] peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-white after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-[#A8A4FF] peer-checked:bg-[#A8A4FF]"></div>
                                     </label>
 
@@ -49,13 +49,13 @@
                                     <label for="amount" class="block mb-1 text-sm">Amount:</label>
                                     <div class="flex flex-row justify-between items-center">
                                         <div class="mb-4 basis-1/4">
-                                            <select id="currency" class="w-full border py-2 rounded focus:border-blue-500 focus:shadow-outline outline-none">
+                                            <select id="currency" v-model="currency" class="w-full border py-2 rounded focus:border-blue-500 focus:shadow-outline outline-none">
                                                 <option value="EUR">EUR</option>
                                                 <option value="USD">USD</option>
                                             </select>
                                         </div>
                                         <div class="mb-4 px-2 basis-3/4">
-                                            <input id="amount" required class="w-full border px-4 py-2 rounded focus:border-blue-500 focus:shadow-outline outline-none" type="text" placeholder="Amount" />
+                                            <input id="amount" v-model="amount" required class="w-full border px-4 py-2 rounded focus:border-blue-500 focus:shadow-outline outline-none" type="text" placeholder="Amount" />
                                         </div>
                                     </div>
                                 </div>
@@ -122,10 +122,10 @@
 </template>
 
 <script setup lang="ts">
-import { ref, reactive } from 'vue';
+import { ref, reactive, watch, onMounted } from 'vue';
 import MultipleInputComponent from './ui/MultipleInputComponent.vue';
 
-defineProps({
+const props = defineProps({
     isUpdateAuctionModalOpen: Boolean,
     auctionData: Object
 });
@@ -138,6 +138,19 @@ let updateFee = ref({
 });
 let existingKeys:String[] = reactive(['one', 'two', 'three'])
 let removedKeys:String[] = reactive([]);
+let autoRenew = ref();
+let currency = ref();
+let amount = ref();
+
+watch(() => props.auctionData, (newAuctionData, oldAuctionData) => {
+    autoRenew = newAuctionData?.node.autoRenew;
+    currency = newAuctionData?.node.price.currency;
+    amount = newAuctionData?.node.price.amount;
+})
+
+onMounted(() => {
+    // call s_keys endpoint with product id from auctionData
+});
 
 function updateNewKey(value:any) {
     newKey.value = value;
