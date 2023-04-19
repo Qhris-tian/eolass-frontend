810fca282aaf70c81d93f34619d6b492533ae8e4
JohnnySimple <obengboatengjohnson@gmail.com>
2023-04-19 00:03:56 +0000

    add: update auction update form

--- src/components/ui/MultipleInputComponent.vue
+++ src/components/ui/MultipleInputComponent.vue
@@ -4,7 +4,7 @@
         <div v-for="(item, index) in items" :key="index" class="w-1/3 p-1">
             <div class="bg-[#A8A4FF] rounded text-white flex justify-between p-1">
                 <span class="">{{ item }}</span>
-                <span class="cursor-pointer" @click="$emit('removeProductKey', index)">X</span>
+                <img src="@/assets/close-icon.svg" @click="$emit('removeProductKey', index)" class="cursor-pointer hover:w-[15px] filter invert transition-all" width="10" height="10" alt="">
             </div>
         </div>
         <input type="text" class="w-1/3 text-sm border-none" placeholder="enter key" v-bind:value="newItem" @input="$emit('updateNewItem', $event.target.value)" @keyup.enter="$emit('addProductKey')">
