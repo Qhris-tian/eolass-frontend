d1c2392a9b59e92b46b4e33c0b387afcc13564df
Qhris-tian <christian.kojobarnes@gmail.com>
2023-04-26 21:58:37 +0000

    Update ProductView.vue

--- src/views/dashboard/ProductView.vue
+++ src/views/dashboard/ProductView.vue
@@ -1,14 +1,13 @@
 <template>
-   <div class="ml-[2rem] md:ml-[12rem]">
-        <Navbar iconName="eneba" v-bind:showSearchBar=true />
-        Products
-   </div>
+  <div class="ml-[2rem] md:ml-[12rem]">
+    <Navbar iconName="eneba" :showSearchBar="true" />
+    <h4 class="heading-xl uppercase">Products</h4>
+  </div>
 </template>
 
 <script setup lang="ts">
-import Navbar from '@/components/Nav-bar.vue'
+import Navbar from '@/components/NavBar.vue'
 </script>
 
 <style scoped>
-
 </style>
