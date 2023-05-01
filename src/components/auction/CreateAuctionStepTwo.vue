fe30de30e4e15dd395eb83d4f4c9b904005e9778
JohnnySimple <obengboatengjohnson@gmail.com>
2023-05-01 11:21:19 +0000

    add: add initial content to create auction step 2

---
+++ src/components/auction/CreateAuctionStepTwo.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="">
+        <div class="w-full flex justify-center">
+            <div class="md:w-[40%]">
+                <SearchBarComponent />
+            </div>
+        </div>
+        <div>
+            <h4>Half Life 3 - Game</h4>
+            <p class="md:w-[40%] flex flex-wrap justify-between font-medium text-[11px] pt-4">
+                <span>EUROPE</span>
+                <span>BULGARIAN</span>
+                <span>LITHUANIAN</span>
+                <span>FRENCH</span>
+            </p>
+        </div>
+        <div class="pt-10">
+            <CompetitionTable />
+        </div>
+    </div>
+</template>
+
+<script setup lang="ts">
+import SearchBarComponent from '@/components/ui/SearchBarComponent.vue';
+import CompetitionTable from '@/components/CompetitionTable.vue';
+
+</script>
+
+<style scoped>
+</style>
--- src/components/auction/GetAuctionProductComponent.vue
+++ src/components/auction/GetAuctionProductComponent.vue
@@ -1,8 +1,11 @@
 <template>
-  <div>Get Auction Product</div>
+  <div class="ml-[2rem] mt-20 mr-10  py-20 px-10 border">
+    <CreateAuctionStepTwo />
+  </div>
 </template>
 
 <script setup lang="ts">
+import CreateAuctionStepTwo from './CreateAuctionStepTwo.vue';
 </script>
 
 <style scoped>
