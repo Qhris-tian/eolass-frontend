95bd3b6924f8bcdb5515caf2dcc156b69627072b
Qhris-tian <christian.kojobarnes@gmail.com>
2023-05-01 13:38:00 +0000

    updated axios config

--- src/configs/request.ts
+++ src/configs/request.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 export default axios.create({
-  baseURL: process.env.VUE_APP_BASE_URL,
-  timeout: parseInt(process.env.VUE_APP_AXIOS_TIMEOUT ?? '15000'),
-//   headers: { 'Authorization': 'foobar' }
+  baseURL: import.meta.env.VITE_APP_BASE_URL,
+  timeout: parseInt(import.meta.env.VITE_APP_AXIOS_TIMEOUT ?? '15000')
+  //   headers: { 'Authorization': 'foobar' }
 })
