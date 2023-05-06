72f896386ede4b00d4b9d24bb0461d29eefb3a55
Qhris-tian <christian.kojobarnes@gmail.com>
2023-05-06 17:27:08 +0000

    Update inventory.ts

--- src/stores/inventory.ts
+++ src/stores/inventory.ts
@@ -2,6 +2,7 @@ import { ref } from 'vue'
 import { defineStore } from 'pinia'
 import axios from '@/configs/request'
 import type { Product } from '@/interfaces/inventory'
+import type { Result } from '@/interfaces/catalog'
 
 export const useInventoryStore = defineStore('inventory', () => {
   const inventory = ref<Array<Product>>([])
@@ -11,5 +12,13 @@ export const useInventoryStore = defineStore('inventory', () => {
       inventory.value = data
     })
   }
-  return { inventory, getInventoryData }
+
+  function createInventory(product: Result) {
+    return axios.post('/api/v1/inventory/', {
+      sku: product.sku,
+      title: product.title,
+      region: product.regions[0]?.name
+    })
+  }
+  return { inventory, getInventoryData, createInventory }
 })
